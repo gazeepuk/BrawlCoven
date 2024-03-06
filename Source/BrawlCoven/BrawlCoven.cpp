efe2c16fc8cbe380c4f389992e0d8cc998c9222d
Gazee Pukolshik <107416973+gazeepuk@users.noreply.github.com>
2024-03-06 19:14:03 +0300

    Init commit

---
+++ Source/BrawlCoven/BrawlCoven.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BrawlCoven.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BrawlCoven, "BrawlCoven" );
